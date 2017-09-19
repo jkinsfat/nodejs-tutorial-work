e6293ad60b94fd58b9071cdc857d595850a39c1c
jkinsfat <jasonrkinsfather@gmail.com>
2017-09-19 15:28:35 -0700

    How to perform an asynchronous test

--- Ch10/10_02/start/test/tools-spec.js
+++ Ch10/10_02/start/test/tools-spec.js
@@ -1,14 +1,25 @@
 var expect = require("chai").expect;
 var tools = require("../lib/tools");
 
-describe("printName()", function() {
+describe("Tools", function() {
+	describe("printName()", function() {
 
-	it("should print the last name first", function() {
+		it("should print the last name first", function() {
 
-		var results = tools.printName({ first: "Alex", last: "Banks"});
+			var results = tools.printName({ first: "Alex", last: "Banks"});
 
-		expect(results).to.equal("Banks, Alex");
+			expect(results).to.equal("Banks, Alex");
+
+		});
 
 	});
 
-});
+	describe("loadWiki()", function() {
+		it("Load Abraham Lincoln's wiki page", function(done) {
+			tools.loadWiki({first: "Abraham", last: "Lincoln"}, function(html) {
+				expect(html).to.be.ok;
+				done();
+			})
+		});
+	})
+})
